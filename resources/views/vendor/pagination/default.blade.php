8660dbcecfb746e5489fd8558ea1a42c32c12503
Ashwani <ashwani90125@gmail.com>
2024-10-04 16:01:14 +0530

    Work on add blogs

--- resources/views/vendor/pagination/default.blade.php
+++ resources/views/vendor/pagination/default.blade.php
@@ -1,56 +1,46 @@
 @if ($paginator->hasPages())
-<div class="container-fluid pag-container">
-<nav aria-label="Page navigation example">
-  <ul class="pagination">
-  @if ($paginator->onFirstPage())
-    <li class="page-item disabled">
-      <a class="page-link" href="{{ $paginator->previousPageUrl() }}" aria-label="Previous" rel="prev" aria-label="@lang('pagination.previous')">
-        <span aria-hidden="true">&laquo;</span>
-        <span class="sr-only">Previous</span>
-      </a>
-    </li>
-   @else
-    <li class="page-item">
-        <a class="page-link" href="#" rel="prev" aria-label="@lang('pagination.prev')">
-        <span aria-hidden="true">&laquo;</span>
-            <span class="sr-only">Previous</span>
-        </a>
-        </a>
-    </li>
-   @endif
-   @foreach ($elements as $element)
+    <nav>
+        <ul class="pagination">
+            {{-- Previous Page Link --}}
+            @if ($paginator->onFirstPage())
+                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
+                    <span class="page-link" aria-hidden="true">&lsaquo;</span>
+                </li>
+            @else
+                <li class="page-item">
+                    <a class="page-link" style="height:30px" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
+                </li>
+            @endif
+
+            {{-- Pagination Elements --}}
+            @foreach ($elements as $element)
                 {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
-                    <li class="disabled page-item" aria-disabled="true"><span>{{ $element }}</span></li>
+                    <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
                 @endif
 
                 {{-- Array Of Links --}}
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <li class="active page-item" aria-current="page"><a href="#" class="page-link">{{ $page }}</a></li>
+                            <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
                         @else
-                            <li class="page-item"><a href="{{ $url }}" class="page-link">{{ $page }}</a></li>
+                            <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
                         @endif
                     @endforeach
                 @endif
             @endforeach
-    @if ($paginator->hasMorePages())
-        <li class="page-item">
-            <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">
-                <span aria-hidden="true">&raquo;</span>
-            <span class="sr-only">Next</span>
-        </a>
-        </li>
-    @else
-        <li class="disabled page-item" aria-disabled="true" aria-label="@lang('pagination.next')">
-        <a class="page-link" href="#" rel="next" aria-label="@lang('pagination.next')">
-        <span aria-hidden="true">&raquo;</span>
-        <span class="sr-only">Next</span>
-        </a>
-        </li>
-    @endif
-  </ul>
-</nav>
-</div>
+
+            {{-- Next Page Link --}}
+            @if ($paginator->hasMorePages())
+                <li class="page-item">
+                    <a class="page-link" style="height:30px" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
+                </li>
+            @else
+                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
+                    <span class="page-link" aria-hidden="true">&rsaquo;</span>
+                </li>
+            @endif
+        </ul>
+    </nav>
 @endif
