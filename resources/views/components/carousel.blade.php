e0d4d4801e61ca0c47c5db3edb3684f34e699b7a
Ashwani <ashwani90125@gmail.com>
2024-12-26 12:35:23 +0530

    Work on add styles to support mobile styles

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,116 +1,61 @@
-<section class="homeHeroCarousel home-banner">
-    <input type="hidden" class="banner-slide-duration" value="7"/>
-<div class="swiper mySwiper home-banner">
-    <div class="swiper-wrapper home-banner-carousel">
-        <div class="swiper-slide home-banner-slider" data-swiper-slide-index="0" role="group" >
-            <div class="home-banner-box">
-                <div class="home-banner-content text-left position-absolute z-index-2" data-dl-title="Ecosystems for mobility ">
-                <h2 class="home-banner-title">Empower Your Evolution: Transform with AI </h2>
-
-                    <div class="text-img-container">
-
-
-                        <div class="home-banner-desc d-none d-lg-block">Unlock the potential of AI to propel your evolution forward. Embrace transformative technologies for a future empowered by innovation.</div>
-                        <a href="https://www.tcs.com/what-we-do/industries/travel-and-logistics" class="tcs-primary-btn d-inline-block tcs-section-click">Learn more
-
-                        </a>
-                    </div>
-                    <div class="home-banner-links">
-                        <div class="img-container">
-
-
-                        <img src="{{ asset('img/images/ai_generated0.jpg') }}"/>
-                        <div class="img-hover-st">
-                        </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="home-banner-progress-bar position-absolute" style="width: 5%;z-index:10;"></div>
-        </div>
-        <div class="swiper-slide home-banner-slider" data-swiper-slide-index="0" role="group" >
-            <div class="home-banner-box">
-                <div class="home-banner-content text-left position-absolute z-index-2" data-dl-title="Ecosystems for mobility ">
-                <h2 class="home-banner-title">Elevate Your Experience: Upgrade Your Software Today </h2>
-                    <div class="text-img-container">
-
-
-
-                        <div class="home-banner-desc d-none d-lg-block">Enhance efficiency and elevate performance with our customized software solutions. Elevate your experience and stay ahead in today's competitive landscape by upgrading your software now.</div>
-                        <a href="https://www.tcs.com/what-we-do/industries/travel-and-logistics" class="tcs-primary-btn d-inline-block tcs-section-click">Learn more
-
-                        </a>
-                    </div>
-                    <div class="home-banner-links">
-                        <div class="img-container">
-
-
-                        <img src="{{ asset('img/images/ai_generated1.jpg') }}" />
-                        <div class="img-hover-st">
-                        </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="home-banner-progress-bar position-absolute" style="width: 5%;z-index:10;"></div>
-        </div>
-        <div class="swiper-slide home-banner-slider" data-swiper-slide-index="0" role="group" >
-            <div class="home-banner-box">
-                <div class="home-banner-content text-left position-absolute z-index-2" data-dl-title="Ecosystems for mobility ">
-                <h2 class="home-banner-title">Maximize Efficiency, Optimize Performance </h2>
-                    <div class="text-img-container">
-
-
-
-                        <div class="home-banner-desc d-none d-lg-block">Unlock peak productivity with our solutions designed to maximize efficiency and optimize performance, empowering you to achieve your goals with precision and effectiveness.</div>
-                        <a href="https://www.tcs.com/what-we-do/industries/travel-and-logistics" class="tcs-primary-btn d-inline-block tcs-section-click">Learn more
-
-                        </a>
-                    </div>
-                    <div class="home-banner-links">
-                        <div class="img-container">
-
-
-
-
-                        <img src="{{ asset('img/images/ai_generated2.jpg') }}" />
-                        <div class="img-hover-st">
-                        </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="home-banner-progress-bar position-absolute" style="width: 5%;z-index:10;"></div>
-        </div>
-        <div class="swiper-slide home-banner-slider" data-swiper-slide-index="0" role="group" >
-            <div class="home-banner-box">
-                <div class="home-banner-content text-left position-absolute z-index-2" data-dl-title="Ecosystems for mobility ">
-                <h2 class="home-banner-title">Tailored Solutions, Crafted for Your Success </h2>
-                    <div class="text-img-container">
-
-
-
-                        <div class="home-banner-desc d-none d-lg-block">Discover bespoke software solutions meticulously designed to elevate your business outcomes. Experience innovation tailored precisely to your needs for unparalleled success.</div>
-                        <a href="https://www.tcs.com/what-we-do/industries/travel-and-logistics" class="tcs-primary-btn d-inline-block tcs-section-click">Learn more
-
-                        </a>
-                    </div>
-                    <div class="home-banner-links">
-                        <div class="img-container">
-
-
-
-                        <img src="{{ asset('img/images/ai_generated.jpg') }}" />
-                        <div class="img-hover-st">
-                        </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="home-banner-progress-bar position-absolute" style="width: 5%;z-index:10;"></div>
-        </div>
-    </div>
-    <span class="swiper-button-next-1"><i class="fa fa-arrow-right"></i></span>
-    <span class="swiper-button-prev-2"><i class=" fa fa-arrow-left"></i></span>
+<section class="own-carousel">
+    
+  <div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
   </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+        <div>
+        <button type="button" class="btn btn-responsive tcs-primary-btn d-inline-block tcs-section-click">Explore</button>
+        </div>
+      </div>
+      <img src="{{ asset('img/images/ai_generated0.jpg') }}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+    <div class="carousel-caption d-none d-md-block">
+    <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+        <div>
+        <button type="button" class="btn btn-responsive tcs-primary-btn d-inline-block tcs-section-click">Explore</button>
+        </div>
+      </div>
+      <img src="{{ asset('img/images/ai_generated1.jpg') }}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+    <div class="carousel-caption d-none d-md-block">
+    <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+        <div>
+        <button type="button" class="btn btn-responsive tcs-primary-btn d-inline-block tcs-section-click">Explore</button>
+        </div>
+      </div>
+      <img src="{{ asset('img/images/ai_generated2.jpg') }}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+    <div class="carousel-caption d-none d-md-block">
+    <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+        <div>
+        <button type="button" class="btn btn-responsive tcs-primary-btn d-inline-block tcs-section-click">Explore</button>
+        </div>
+      </div>
+      <img src="{{ asset('img/images/ai_generated.jpg') }}" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 </section>
